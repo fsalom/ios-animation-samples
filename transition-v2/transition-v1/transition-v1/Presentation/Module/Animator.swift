4c2906132f039e0bf8004d8aa273dbbe0dfc01ec
Fernando Salom <Fdosalom@gmail.com>
2022-11-01 20:18:51 +0100

    Remove unnecessary files

--- transition-v2/transition-v1/transition-v1/Presentation/Module/Animator.swift
+++ transition-v2/transition-v1/transition-v1/Presentation/Module/Animator.swift
@@ -7,11 +7,7 @@
 
 import UIKit
 
-// 8
 final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
-
-    // 9
-
     static let duration: TimeInterval = 0.5
 
     private let type: PresentationType
@@ -20,8 +16,6 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
     private var selectedCellImageViewSnapshot: UIView
     private let cellImageViewRect: CGRect
 
-
-    // 10
     init?(type: PresentationType, firstViewController: ListController, secondViewController: DetailController, selectedCellImageViewSnapshot: UIView) {
         self.type = type
         self.firstViewController = firstViewController
@@ -32,23 +26,16 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
             let selectedCell = firstViewController.selectedCell
             else { return nil }
 
-        // 11
         self.cellImageViewRect = selectedCell.userImage.convert(selectedCell.userImage.bounds, to: window)
-
-
     }
 
-    // 12
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return Self.duration
     }
 
-    // 13
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        // 18
         let containerView = transitionContext.containerView
 
-        // 19
         guard let toView = secondViewController.view
             else {
                 transitionContext.completeTransition(false)
@@ -57,7 +44,6 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
 
         containerView.addSubview(toView)
 
-        // 21
         guard
             let selectedCell = firstViewController.selectedCell,
             let window = firstViewController.view.window ?? secondViewController.view.window,
@@ -71,16 +57,12 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
 
         let isPresenting = type.isPresenting
 
-        // 40
         let backgroundView: UIView
         let fadeView = UIView(frame: containerView.bounds)
         fadeView.backgroundColor = secondViewController.view.backgroundColor
 
-        // 33
         if isPresenting {
             selectedCellImageViewSnapshot = cellImageSnapshot
-
-            // 41
             backgroundView = UIView(frame: containerView.bounds)
             backgroundView.addSubview(fadeView)
             fadeView.alpha = 0
@@ -89,79 +71,51 @@ final class Animator: NSObject, UIViewControllerAnimatedTransitioning {
             backgroundView.addSubview(fadeView)
         }
 
-        // 23
         toView.alpha = 0
-
-        // 34
-        // 42
-        // 48
-        // 54
         [backgroundView, selectedCellImageViewSnapshot, controllerImageSnapshot].forEach { containerView.addSubview($0) }
 
-        // 25
         let controllerImageViewRect = secondViewController.backgroundImage.convert(secondViewController.backgroundImage.bounds, to: window)
-        // 35
+
         [selectedCellImageViewSnapshot, controllerImageSnapshot].forEach {
             $0.frame = isPresenting ? cellImageViewRect : controllerImageViewRect
-
-            // 59
             $0.layer.cornerRadius = isPresenting ? 12 : 0
             $0.layer.masksToBounds = true
         }
 
-        // 36
         controllerImageSnapshot.alpha = isPresenting ? 0 : 1
 
-        // 37
         selectedCellImageViewSnapshot.alpha = isPresenting ? 1 : 0
 
-        // 27
         UIView.animateKeyframes(withDuration: Self.duration, delay: 0, options: .calculationModeCubic, animations: {
 
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
-                // 38
                 self.selectedCellImageViewSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
                 controllerImageSnapshot.frame = isPresenting ? controllerImageViewRect : self.cellImageViewRect
-
-                // 43
                 fadeView.alpha = isPresenting ? 1 : 0
 
-
-                // 60
                 [controllerImageSnapshot, self.selectedCellImageViewSnapshot].forEach {
                     $0.layer.cornerRadius = isPresenting ? 0 : 12
                 }
             }
 
-            // 39
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.6) {
                 self.selectedCellImageViewSnapshot.alpha = isPresenting ? 0 : 1
                 controllerImageSnapshot.alpha = isPresenting ? 1 : 0
             }
 
         }, completion: { _ in
-            // 29
             self.selectedCellImageViewSnapshot.removeFromSuperview()
             controllerImageSnapshot.removeFromSuperview()
-
-            // 44
             backgroundView.removeFromSuperview()
-
-            // 30
             toView.alpha = 1
-
-            // 31
             transitionContext.completeTransition(true)
         })
     }
 }
 
-// 14
 enum PresentationType {
-
     case present
     case dismiss
-
     var isPresenting: Bool {
         return self == .present
     }
